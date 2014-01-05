313fca5f97e5eeec7dfa45dbc2f968b85274b787
hr19900125 <hr19900125@126.com>
2014-01-06 00:16:59 +0800

    add FutureDemo

---
+++ JaveDemo/src/com/ryan/java/concurrent/FutureDemo.java
@@ -0,0 +1,31 @@
+package com.ryan.java.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureDemo {
+	public static class Task implements Callable<String> {
+		@Override
+		public String call() throws Exception {
+			String tid = String.valueOf(Thread.currentThread().getId());
+			System.out.printf("Thread#%s : in call\n", tid);
+			return tid;
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException,
+			ExecutionException {
+		List<Future<String>> results = new ArrayList<Future<String>>();
+		ExecutorService es = Executors.newCachedThreadPool();
+		for (int i = 0; i < 100; i++)
+			results.add(es.submit(new Task()));
+		for (Future<String> res : results)
+			System.out.println(res.get());
+	}
+
+}
