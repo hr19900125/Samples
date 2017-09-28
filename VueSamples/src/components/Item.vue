52ff0ffd408fdbc8572cc5a702ae19f834b166ee
黄睿 <huangrui@meizu.com>
2017-09-28 11:01:00 +0800

    add vuejs demo

---
+++ VueSamples/src/components/Item.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <a href="#">{{item.name}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["item"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+  display: block;
+}
+</style>
---
+++ VueSamples/src/views/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Demo</h1>
+    <ul>
+      <li v-for="item in samples">
+        <sample-item v-bind:item="item"></sample-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import data from "../../data";
+import Item from "../components/Item";
+
+export default {
+  components: {
+     "sample-item":Item
+  },
+  data () {
+    return {
+      samples:[]
+  }},
+  created (){
+    var me = this;
+    me.samples = data.samples;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
